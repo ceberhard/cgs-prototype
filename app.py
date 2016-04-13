b659970b9085bebe94cc392545c1fdc3f56b1669
ceberhard <ceberhar@mail.usf.edu>
2016-04-13 03:08:17 -0400

    Working on logic for cards and deck

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ def main():
     playercards = Deck()
     print('Player Deck:')
     print(playercards.deck)
-
-
-
+    playercards.shuffle()
+    print('Shuffled:')
+    print(playercards.deck)
+    hand = playercards.draw(3)
+    print('Hand:')
+    print(hand)
+    print('New Deck')
+    print(playercards.deck)
 
 if __name__ == '__main__':
     main()
--- lib/cards.py
+++ lib/cards.py
@@ -1,11 +1,34 @@
+import uuid
+import collections
+
+class Card:
+    def __init__(self, suit, numval):
+        self.suit = suit
+        self.numval = numval
+        self.__cardnames = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king']
+        self.name = str(self.__cardnames[numval])
+
+    def __repr__(self):
+        return '{0} of {1}'.format(self.name, self.suit)
+
+    def __str__(self):
+        return '{0} of {1}'.format(self.name, self.suit)
 
 class Deck:
     def __init__(self):
-        self.__initialize_deck()
+        self.deck = collections.deque(self.__create_deck())
 
-    def __initialize_deck(self):
+    def shuffle(self):
+        prepdeck = dict([(uuid.uuid4(), card) for card in self.deck])
+        shuffledeck = collections.OrderedDict(sorted(prepdeck.items()))
+        self.deck = collections.deque([shuffledeck[key] for key in shuffledeck.keys()])
+
+    def draw(self, handsize):
+        return [self.deck.popleft() for i in range(handsize)]
+
+    def __create_deck(self):
         suits = ['clubs','diamonds','hearts','spades']
-        self.deck = [(suit, val + 1) for suit in suits for val in range(13)]
+        return [Card(suit, val) for suit in suits for val in range(13)]
 
 
 
